9c3fe77d79eae1d64b89df7342aaeccb6bd43f14
CHristof <kaufen@cmja.de>
2022-08-20 21:38:58 +0200

    schnelle Abfrage

--- myTimers.cpp
+++ myTimers.cpp
@@ -8,7 +8,7 @@
 //101: 1000 ms
 
 volatile TIMER MyTimers[MYTIMER_NUM]= {	{TM_START,RESTART_YES,60,0,nextCapaMeasure},
-                                        {TM_START,RESTART_YES,400,0,nextCapaSend},
+                                        {TM_START,RESTART_YES,100,0,nextCapaSend},
                                         {TM_STOP,RESTART_NO,5000,0,stopRelais},
                                         {TM_START,RESTART_NO,5000,0,startupTimer},
                                         {TM_START,RESTART_YES,200,0,nextTempMeasure},
